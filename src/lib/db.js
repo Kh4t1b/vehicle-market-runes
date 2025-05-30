61168d07febe90c7396a363c4e44e3dfffe4e9ea
Kh4tib <khatib100601@gmail.com>
2025-05-30 20:01:15 +0200

    Fahrzeugformular mit Bild-Upload integriert

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,5 +3,5 @@ import { MongoClient } from 'mongodb';
 const client = new MongoClient(import.meta.env.VITE_DB_URI);
 await client.connect();
 
-const db = client.db('vehicle-market');
+const db = client.db(); // << kein 'vehiclemarket' hardcoden, wenn schon in URI
 export const cars = db.collection('cars');
